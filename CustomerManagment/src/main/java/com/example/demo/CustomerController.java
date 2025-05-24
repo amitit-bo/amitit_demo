12f8af40f2f263fa406ee0bc3466d1fa8d8bcb27
admin@avsoftechnologies.com <BXLinx!6>
2025-05-24 10:12:25 +0530

    added on rest api

--- CustomerManagment/src/main/java/com/example/demo/CustomerController.java
+++ CustomerManagment/src/main/java/com/example/demo/CustomerController.java
@@ -1,5 +1,20 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
 
+	@GetMapping("/hello")
+	public String m1()
+	{
+		return  "abc";
+		
+	}
+	
+	
+	
+	
+	
 }
